b4b35d4c058d84bfd37554b4bfe54b8c3c87ce60
sairam9640 <sairamganapavarapu@gmail.com>
2025-03-31 14:30:31 +0530

    first commit

---
+++ CompanyInfo.js
@@ -0,0 +1,87 @@
+export const companyInfo = `
+Introduction:
+I'm your friendly AI chatbot, here to assist you with anything related to Sairam Ganapavarapu, I'm here to help.
+
+Details:
+ I am an Entry-Level Java Full-Stack Developer with a strong foundation in Java, Spring Boot, React.js,
+ and MySQL. With a passion for building dynamic web applications, I have worked on projects like a 
+ Book Management System and a Blog Application, optimizing data retrieval and enhancing user 
+ experience.
+
+I have recently completed my B.Tech in Electronics and Communication Engineering (ECE) from Lakireddy 
+Bali Reddy College of Engineering and have undergone Java Full-Stack training at Digit Institute. 
+My expertise lies in creating scalable, responsive, and high-performing web applications.
+
+Technical Skills:
+
+Programming Languages: Java, Python, JavaScript, SQL
+
+Web Technologies: HTML, CSS, Bootstrap
+
+Databases: MySQL
+
+Frameworks & Libraries: Spring Boot, React.js, Hibernate, Node.js
+
+Development Tools: VS Code, Eclipse Neon, Notepad++
+
+Version Control: Git, GitHub
+
+Other Skills: Object-Oriented Programming (OOP), API Integration
+
+Projects:
+Book Management System (Java, Spring Boot, MySQL, Thymeleaf)
+Developed a CRUD-based book tracking system to enhance library management.
+
+Integrated JPA APIs for efficient database operations, improving data retrieval speed by 30%.
+
+Designed an intuitive UI using Bootstrap to improve user experience and minimize input errors.
+
+Blog Application (React.js, Spring Boot, MySQL, Java)
+Built a full-stack blog application with interactive UI and seamless backend integration.
+
+Implemented CRUD functionalities for blog creation, updates, and management.
+
+Enhanced user experience using Bootstrap components and CSS styling.
+
+Education:
+
+B.Tech in Electronics & Communication Engineering (2021 – 2024)
+Lakireddy Bali Reddy College of Engineering, Andhra Pradesh
+
+Board of Intermediate Education (2018 – 2020)
+Vidya Vikas Junior College, Jangareddygudem
+
+Secondary School Certificate (SSC) (2017 – 2018)
+Sri Chaitanya School, Jangareddygudem
+
+Certifications & Achievements:
+
+JPMorgan Chase & Co. Software Engineering Certification (Forage)
+
+AWS Cloud Computing Training & Certification (APSSDC)
+
+SQL & Relational Databases 101 Certification (Cognitive Class)
+
+AI Training Certification (JNTUK Workshop)
+
+Research Paper: Advanced Leaf Disease Detection and Robotic Pest Management System
+
+
+Networking & Contact:
+
+Stay connected with me :
+
+Portfolio: sairamganapavarapu.netlify.app
+
+GitHub: github.com/Sairam9542
+
+LinkedIn: linkedin.com/in/sairamganapavarapu96
+
+Email: sairamganapavarapu@gmail.com
+
+Career Goals & Aspirations:
+I am actively seeking Java Full-Stack Developer, Web Frontend Engineer, and GIS-related roles where 
+I can contribute my technical skills and innovative thinking. My goal is to work on scalable, 
+real-world applications and enhance my expertise in cloud computing, full-stack development, and 
+data-driven applications.
+`;
---
+++ README.md
@@ -0,0 +1,48 @@
+# AI-CHATBOT🤖
+
+Welcome to My  AiChatbot Repo!
+
+### Weblink: [Live Website]()
+## Some Images:
+<img width="450px;" src="https://res.cloudinary.com/dz1zt2xfc/image/upload/v1743404316/Screenshot_2025-03-31_122714_tdzkwm.png"/>
+
+## Table of Contents
+- [Introduction](#introduction)
+- [Features](#features)
+- [Technologies Used](#technologies-used)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+
+## Introduction
+This repository contains an AI-powered chatbot designed to assist users and providing guidance on the given input data of any Company. Whether you're a beginner or an experienced, this chatbot helps answer queries,we can change the data which AI responds on by giving our data or our Company details.
+
+## Features
+- Provides solutions for Any Queries Based on the Input data.
+- Assists in queries of the Users.
+- Fetches the data given and Never out of bound rather than data.
+- This bot is useful to Companies for Best Ai assist.
+
+
+## Technologies Used
+- React js: It Framework used to create Frontend part of Applications.
+- API : In this I had used an Application Programming Interface of Gemini 2.0 flash which fetches from API key.
+- HTML5 & CSS3: Markup and styling languages for building web pages.
+- JavaScript: A programming language for adding interactivity to web applications.
+- Vite: In a nutshell, Vite is a modern JavaScript build tool that provides out-of-the-box support for common web patterns, offering optimized builds with           the flexibility and maturity of rollup paired with a snappy bundleless dev server made possible by esbuild and native ES modules.
+- Git: A version control system for tracking changes and collaborating on projects.
+
+## Installation
+To run this AI chatbot locally, follow these steps:
+
+1. Clone the repository: `git clone https://github.com/Sairam9542/Ai-Chatbot.git`
+2. Navigate to the project directory.
+3. Install the dependencies: `npm install`
+4. Start the development server: `npm run dev`
+5. Open your browser and visit or it redirects to webpage: `http://localhost:5174/`
+
+## Usage
+After installing and running the project successfully you can navigate through the webpage.Which shows the Chaticon on clicking on it It Shows the AI chatbot Assistance.
+
+## License
+This project is licensed under the [MIT License](LICENSE).
